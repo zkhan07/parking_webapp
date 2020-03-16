d02630fef49ab8d800d0cf4b12106fca24b5c1a4
zaid <khanzaid786786.kz@gmail.com>
2020-03-16 15:18:37 +0530

    Updates

---
+++ precache-manifest.943b5cbf59ab09a6c0467e55e1321636.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88faef7bceb9111e19b584bb46a919ba",
+    "url": "/parking_webapp/index.html"
+  },
+  {
+    "revision": "5633b1dcfbcad6484a17",
+    "url": "/parking_webapp/static/css/2.91e1d17c.chunk.css"
+  },
+  {
+    "revision": "5d84fb6e02d3ee325244",
+    "url": "/parking_webapp/static/css/main.a6f76870.chunk.css"
+  },
+  {
+    "revision": "5633b1dcfbcad6484a17",
+    "url": "/parking_webapp/static/js/2.e70e433e.chunk.js"
+  },
+  {
+    "revision": "caa21e43d8320aa4e7cbf3a0ea743a57",
+    "url": "/parking_webapp/static/js/2.e70e433e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d84fb6e02d3ee325244",
+    "url": "/parking_webapp/static/js/main.cfa7abc3.chunk.js"
+  },
+  {
+    "revision": "deca63edcd5a39f8501b",
+    "url": "/parking_webapp/static/js/runtime-main.7b7c3c6b.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/parking_webapp/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/parking_webapp/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/parking_webapp/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/parking_webapp/static/media/slick.f97e3bbf.svg"
+  }
+]);
